8e04438880baa4f66d769c56334fce7f0a9c447c
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2020-11-23 06:32:29 +0300

    initial project setup is done.

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routers/tourRoutes');
+const userRouter = require('./routers/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+app.use(express.json());
+app.use(express.static(`${__dirname}/../public`));
+app.use(morgan('dev'));
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
