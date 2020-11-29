a5872b410e8888e4390f7a645b1e03eac7987d7a
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2020-11-29 14:13:08 +0300

    requiring db connection via mongoose.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const morgan = require('morgan')
+require('./db/mongoose')
 
-const tourRouter = require('./routers/tourRoutes');
-const userRouter = require('./routers/userRoutes');
+const tourRouter = require('./routers/tourRoutes')
+const userRouter = require('./routers/userRoutes')
 
-const app = express();
+const app = express()
 
 // 1) MIDDLEWARES
-app.use(express.json()); // make body available on req
-app.use(express.static(`${__dirname}/../public`));
-app.use(morgan('dev'));
+app.use(express.json()) // make body available on req
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(express.static(`${__dirname}/../public`))
+    app.use(morgan('dev'))
+}
 
 // 2) ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
-module.exports = app;
+module.exports = app
