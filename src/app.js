859dc8b22d5c3e8f974e206f273dc0638b96dd99
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2020-12-14 21:35:15 +0300

    global error controller, undefined route handler.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 require('./db/mongoose')
 
+const APPError = require('./utils/APPError')
+const globalErrorController = require('./controllers/errorController')
 const tourRouter = require('./routers/tourRoutes')
 const userRouter = require('./routers/userRoutes')
 
@@ -18,5 +20,13 @@ if (process.env.NODE_ENV === 'development') {
 // 2) ROUTES
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+// undefined route handler
+app.all('*', (req, res, next) => {
+    next(new APPError(`Can't get ${req.originalUrl}`, 404))
+})
+
+// global error handler
+app.use(globalErrorController)
 
 module.exports = app
