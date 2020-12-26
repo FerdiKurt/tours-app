77698a6333d0fa1cbd97e24fad6194d2963c8d28
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2020-12-27 02:33:54 +0300

    'unhandledRejection', 'uncaughtException' handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const app = require('./app')
 
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT REJECTION! 💥 Shutting down...')
+    console.log(err)
+    process.exit(1)
+})
+
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`)
+})
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...')
+    console.log(err)
+
+    server.close(() => {
+        process.exit(1)
+    })
 })
