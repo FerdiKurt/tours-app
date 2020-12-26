36f3c51099694240b1cb9a86c9df148bc508ab63
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2020-12-27 02:35:43 +0300

    addition on global error handler.

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -6,7 +6,8 @@ const handleCastErrorDB = err => {
 }
 
 const handleDuplicateFieldsDB = err => {
-    const message = `Duplicate field value: ${err.keyValue['name']}. Please use another value!`
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Duplicate field value: ${value}. Please use another value!`
     return new AppError(message, 400)
 }
 
@@ -16,6 +17,13 @@ const handleValidationError = err => {
     return new AppError(message, 400)
 }
 
+const handleJWTError = () =>
+    new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+    new AppError('Your token has expired. Please login again!', 401);
+
+// development mode error handling route
 const sendErrDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -28,9 +36,6 @@ const sendErrDev = (err, res) => {
 const sendErrProd = (err, res) => {
     // operational, trusted error: send message to client
     if (err.isOperational) {
-        if (!err.message) {
-            err.message = 'Something is wrong with the provided request'
-        }
         res.status(err.statusCode).json({
             status: err.status,
             message: err.message,
@@ -38,7 +43,7 @@ const sendErrProd = (err, res) => {
     }
     // programming or unknown error: don't leak error details
     else {
-        console.error('ERROR', err._message)
+        console.error('ERROR', err.name)
         res.status(500).json({
             status: 'error',
             message: 'Something went wrong!',
@@ -53,16 +58,27 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrDev(err, res)
     } else if (process.env.NODE_ENV === 'production') {
-        let occuredError = { ...err }
-
-        if (occuredError.reason) {
-            occuredError = handleCastErrorDB(occuredError)
-        } else if (occuredError.code === 11000) {
-            occuredError = handleDuplicateFieldsDB(occuredError)
-        } else if ((occuredError._message = 'Validation failed')) {
-            occuredError = handleValidationError(occuredError)
+      
+        let error = Object.create(err)
+        
+        // todo: validation error got problems in production mode
+        // if (error.name = 'ValidationError') {
+        //     error = handleValidationError(error)
+        // }
+       
+        if (error.name === 'JsonWebTokenError'){ 
+            error = handleJWTError();
         }
-
-        sendErrProd(occuredError, res)
+        if (error.name === 'TokenExpiredError'){ 
+            error = handleJWTExpiredError();
+        }
+        if (error.message.includes('Cast')) {
+            error = handleCastErrorDB(error)
+        }
+        if (error.code === 11000) {
+            error = handleDuplicateFieldsDB(error)
+        }
+    
+        sendErrProd(error, res)
     }
 }
