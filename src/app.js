2dc21c4b098e2abce08e179a3472387066566782
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2021-01-01 21:37:09 +0300

    rate limit middleware.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 require('./db/mongoose')
 
 const APPError = require('./utils/APPError')
@@ -9,13 +10,21 @@ const userRouter = require('./routers/userRoutes')
 
 const app = express()
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 app.use(express.json()) // make body available on req
 
 if (process.env.NODE_ENV === 'development') {
     app.use(express.static(`${__dirname}/../public`))
     app.use(morgan('dev'))
 }
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP, try again in an hour!'
+})
+
+app.use('/api', limiter)
 
 // 2) ROUTES
 app.use('/api/v1/tours', tourRouter)
