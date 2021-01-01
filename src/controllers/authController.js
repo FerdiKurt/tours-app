be4884d7770dccc1afbd2f42ce467e31c4c0ce05
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2021-01-01 21:38:56 +0300

    setting  cookie and its options.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -14,7 +14,23 @@ const signToken = id => {
 
 const createAndSendToken = (user, statusCode, res) => {
     const token = signToken(user._id)
-  
+
+    const cookieOpts = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        cookieOpts.secure = true
+    }
+    
+    res.cookie('jwt', token, cookieOpts)
+
+    // remove password from the output
+    user.password = undefined
+
     res.status(statusCode).json({
         status: 'success',
         token,
@@ -25,6 +41,7 @@ const createAndSendToken = (user, statusCode, res) => {
 }
 
 exports.signup = catchAsync(async (req, res, next) => {
+    // TODO: change this part
     const newUser = await User.create(req.body)
     createAndSendToken(newUser, 201, res)      
 })
