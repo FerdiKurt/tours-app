dca08b82fc94573225b90265177a10af067c9491
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2021-01-21 22:41:00 +0300

    refactor on user controller.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/APPError')
+const factory = require('./handlerFactory')
 
 const filterObj = (obj, ...opts) => {
     const newObj = {}
@@ -14,75 +15,52 @@ const filterObj = (obj, ...opts) => {
     return newObj
 }
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find()
-
-  // send response
-  res.status(200).json({
-      status: 'success',
-      results: users.length,
-      data: {
-        users
-      },
-  })
-})
-
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-}
-
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
-  });
-}
-
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-}
-
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not defined! Please use /signup instead!'
   });
 }
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-    // create error if user posts password data
-    if (req.body.password || req.body.passwordConfirm) {
-        return next(new AppError(
-            'This route is not for password updates!', 
-            400
-        ))
-    }
+  // create error if user posts password data
+  if (req.body.password || req.body.passwordConfirm) {
+      return next(new AppError(
+          'This route is not for password updates!', 
+          400
+      ))
+  }
 
-    // update user document, filter out unwanted fields
-    const filteredBody = filterObj(req.body, 'name', 'email')
-    const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
-        new: true,
-        runValidators: true
-    })
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user: updatedUser
-        }
-    })
+  // update user document, filter out unwanted fields
+  const filteredBody = filterObj(req.body, 'name', 'email')
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+      new: true,
+      runValidators: true
+  })
+  res.status(200).json({
+      status: 'success',
+      data: {
+          user: updatedUser
+      }
+  })
 })
 
+exports.getMe = (req, res, next) => {
+    req.params.id = req.user.id
+
+    next()
+}
 exports.deleteMe = catchAsync(async (req, res, next) => {
-    await User.findByIdAndUpdate(req.user._id, { active: false })
+  await User.findByIdAndUpdate(req.user._id, { active: false })
 
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
+  res.status(204).json({
+      status: 'success',
+      data: null
+  })
 })
+
+// Do NOT update password with this
+exports.updateUser = factory.updateOne(User)
+exports.deleteUser = factory.deleteOne(User)
+exports.getAllUsers = factory.getAll(User)
+exports.getUser = factory.getOne(User)
