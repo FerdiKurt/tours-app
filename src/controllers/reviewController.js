bac43caa2cc1ace9bd34012ff064f493d4ad0da0
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2021-01-21 22:40:42 +0300

    refactor on review controller.

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,43 +1,19 @@
 const Review = require('../models/reviewModel')
-const APIFeatures = require('../utils/APIFeatures')
-const catchAsync = require('../utils/catchAsync')
-const AppError = require('../utils/APPError')
+// const APIFeatures = require('../utils/APIFeatures')
+// const catchAsync = require('../utils/catchAsync')
+// const AppError = require('../utils/APPError')
+const factory = require('./handlerFactory')
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-    let filter = {}
-    if (req.params.tourId) filter = { tour: req.params.tourId }
-
-    const features = new APIFeatures(Review.find(filter), req.query)
-        .filter()
-        .sort()
-        .limit()
-        .paginate()
-    
-    const reviews = await features.query
-
-    // send response
-    res.status(200).json({
-        status: 'success',
-        results: reviews.length,
-        data: {
-            reviews
-        }
-    })
-
-})
-
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setIds = (req, res, next) => {
     // allow nested routes
     if (!req.body.tour) req.body.tour = req.params.tourId
     if (!req.body.user) req.body.user = req.user.id
-    // console.log(req.user)
+    
+    next()
+}
 
-    const newReview = await Review.create(req.body)
-
-    res.status(201).json({
-        status: 'success',
-        data: {
-            review: newReview
-        }
-    })
-})
+exports.getAllReviews = factory.getAll(Review)
+exports.getReview = factory.getOne(Review)
+exports.createReview = factory.createOne(Review)
+exports.updateReview = factory.updateOne(Review)
+exports.deleteReview = factory.deleteOne(Review)
