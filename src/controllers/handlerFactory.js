51a667f4365146dfa8d7a02c6c2f013a35d7867f
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2021-01-21 22:43:39 +0300

    ignore this.

--- src/controllers/handlerFactory.js
+++ src/controllers/handlerFactory.js
@@ -2,40 +2,6 @@ const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/APPError')
 const APIFeatures = require('../utils/APIFeatures')
 
-exports.deleteOne = Model => catchAsync(async (req, res, next) => {
-    const _id = req.params.id
-
-    const doc = await Model.findByIdAndDelete(_id)
-    if (!doc) {
-        return next(new AppError('No document found with that ID', 404))
-    }
-
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    })
-})
-
-exports.updateOne = Model => catchAsync(async (req, res, next) => {
-    const _id = req.params.id
-
-    const doc = await Model.findByIdAndUpdate(_id, req.body, {
-        new: true,
-        runValidators: true,
-    })
-
-    if (!doc) {
-        return next(new AppError('No document found with that ID', 404))
-    }
-
-    res.status(201).json({
-        status: 'success',
-        data: {
-            data: doc,
-        },
-    })
-})
-
 exports.createOne = Model => catchAsync(async (req, res, next) => {
     const doc = await Model.create(req.body)
 
@@ -90,4 +56,38 @@ exports.getAll = Model => catchAsync(async (req, res, next) => {
           data: docs,
       },
   })
+})
+
+exports.deleteOne = Model => catchAsync(async (req, res, next) => {
+    const _id = req.params.id
+
+    const doc = await Model.findByIdAndDelete(_id)
+    if (!doc) {
+        return next(new AppError('No document found with that ID', 404))
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    })
+})
+
+exports.updateOne = Model => catchAsync(async (req, res, next) => {
+    const _id = req.params.id
+
+    const doc = await Model.findByIdAndUpdate(_id, req.body, {
+        new: true,
+        runValidators: true,
+    })
+
+    if (!doc) {
+        return next(new AppError('No document found with that ID', 404))
+    }
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            data: doc,
+        },
+    })
 })
