2d535674458268b4227cc5157a4266af48cce587
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2021-01-24 03:26:07 +0300

    adding ratingAverage and ratingsQuantity.

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Tour = require('./tourModel')
 
 const reviewSchema = new mongoose.Schema({
         review: {
@@ -41,6 +42,57 @@ reviewSchema.pre(/^find/, function (next) {
     })
 
     next()
+})
+
+// static methods
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    const reviewModel = this
+    const stats = await reviewModel.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: { 
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating'}
+            }
+        }
+    ])
+
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        })
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4,
+        })
+    }
+}
+
+// post middleware does not access to the 'next'
+reviewSchema.post('save', function () {
+    // this points current review
+    this.constructor.calcAverageRatings(this.tour)
+})
+
+// query middleware: find()
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    this.reviewDocument = await this.findOne()
+    next()
+})
+
+reviewSchema.post(/^findOneAnd/, async function () {
+    // below does NOT work, query has already executed
+    // this.reviewDocument = await this.findOne()
+    await this.reviewDocument.constructor.calcAverageRatings(
+        this.reviewDocument.tour
+    )
 })
 
 const Review = mongoose.model('Review', reviewSchema)
