e49ef04c10b373f06b139a57a6bd3fb3675ec9be
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2021-01-28 03:25:27 +0300

    get tours within given distance.

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -1,7 +1,6 @@
 const Tour = require('../models/tourModel')
 const catchAsync = require('../utils/catchAsync')
-// const AppError = require('../utils/APPError')
-// const APIFeatures = require('../utils/APIFeatures')
+const AppError = require('../utils/APPError')
 const factory = require('./handlerFactory')
 
 exports.getFiveCheap = (req, res, next) => {
@@ -78,6 +77,41 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
         status: 'success',
         data: {
             plan,
+        },
+    })
+})
+
+// /tours-within/:distance/center/:latlng/unit/:unit
+// /tours-within/distance/100/center/-60,120/unit/:km
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+    const { distance, latlng, unit } = req.params
+    const [lat, lng] = latlng.split(',')
+    const radius =
+        (unit === 'mi' ? distance / 3963.2 : distance / 6378.1);
+
+    if (!lat || !lng) {
+        next(
+            new AppError(
+                'Please provide latitude and longitude!',
+                400
+            )
+        )
+    }
+
+    const tours = await Tour.find({
+            startLocation: {
+                $geoWithin: { 
+                    $centerSphere: [[lng, lat], radius]        
+                }
+            }
+        }
+    )
+    
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            data: tours,
         },
     })
 })
